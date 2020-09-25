b0a73797c748c73034f8c2240e65364a33364292
kozhyx <kozhyx63@outlook.com>
2020-09-25 13:42:08 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+http.listen(3000, () => {
+});
